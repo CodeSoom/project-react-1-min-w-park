7c1502358827682902179daea39dcf39d4f85f26
Minwook Park <mwpark0975@gmail.com>
2020-07-31 18:42:30 +0900

    renaming test label

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,12 +6,33 @@ import { MemoryRouter } from 'react-router-dom';
 
 import App from './App';
 
-test('App', () => {
-  const { container } = render((
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>
-  ));
+describe('App', () => {
+  function renderApp({ path }) {
+    return render((
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>
+    ));
+  }
 
-  expect(container).toHaveTextContent('함께 회,고(Go) 해요.');
+  it('renders App', () => {
+    const { container } = renderApp({ path: '/' });
+    expect(container).toHaveTextContent('함께 회,고(Go) 해요.');
+  });
+
+  context('with path /', () => {
+    it('renders the initial Retrospectives page', () => {
+      const { container } = renderApp({ path: '/' });
+
+      expect(container).toHaveTextContent('무엇을 하셨나요?');
+    });
+  });
+
+  context('with path /writes', () => {
+    it('renders the writing page', () => {
+      const { container } = renderApp({ path: '/writes' });
+
+      expect(container).toHaveTextContent('회고 작성');
+    });
+  });
 });
--- src/RetrospectivesPage.jsx
+++ src/RetrospectivesPage.jsx
@@ -4,6 +4,7 @@ import RetrospectivesContainer from './RetrospectivesContainer';
 export default function RetrospectivesPage() {
   return (
     <div>
+      <h3>무엇을 하셨나요?</h3>
       <RetrospectivesContainer />
     </div>
   );
