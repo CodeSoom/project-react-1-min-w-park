82969fb8f24632832d044731ab0dbeeba9b7a426
Minwook Park <mwpark0975@gmail.com>
2020-08-01 16:43:46 +0900

    Apply styled for CSS

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,52 @@ import {
   Link,
 } from 'react-router-dom';
 
+import styled from '@emotion/styled';
+import { Global, css } from '@emotion/core';
+
 import RetrospectivesPage from './RetrospectivesPage';
 import WritingPage from './WritingPage';
 
+const GlobalStyles = css`
+  @font-face { 
+    font-family: 'MapoFlowerIsland'; 
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/MapoFlowerIslandA.woff') 
+    format('woff'); 
+    font-weight: normal; 
+    font-style: normal; 
+  }
+
+  * {
+    font-family: 'MapoFlowerIsland';
+  }
+`;
+
+const Header = styled.div({
+  margin: '5em 10em',
+});
+
 export default function App() {
   return (
-    <div>
-      <h1>
-        <Link to="/">함께 회,고(Go) 해요.</Link>
-      </h1>
-      <h2>
-        회고를 작성하고, 같은 고민을 가진 사람들과 나누어 보세요.
-      </h2>
-      <div>
-        <ul>
-          <li><Link to="/">회고</Link></li>
-          <li><Link to="/writes">작성</Link></li>
-        </ul>
-      </div>
-
+    <>
+      <Global styles={GlobalStyles} />
+      <Header>
+        <h1>
+          <Link to="/">함께 회,고(Go) 해요.</Link>
+        </h1>
+        <h2>
+          회고를 작성하고, 같은 고민을 가진 사람들과 나누어 보세요.
+        </h2>
+        <div>
+          <ul>
+            <li><Link to="/">회고</Link></li>
+            <li><Link to="/writes">작성</Link></li>
+          </ul>
+        </div>
+      </Header>
       <Switch>
         <Route exact path="/" component={RetrospectivesPage} />
         <Route exact path="/writes" component={WritingPage} />
       </Switch>
-    </div>
+    </>
   );
 }
--- src/RetrospectivesContainer.jsx
+++ src/RetrospectivesContainer.jsx
@@ -2,43 +2,87 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 
+import styled from '@emotion/styled';
+
 import { get } from './utils';
 
 import retrospectives from '../fixtures/retrospectives';
+
+// import {
+//   lightShadeGray,
+//   black,
+// } from './styles/constants';
+
+const List = styled.ul({
+  margin: '3em 10em',
+  padding: '.4em 0',
+  listStyle: 'none',
+});
+
+const Item = styled.li({
+  marginRight: '1em',
+  padding: '1.4em 1em',
+  borderBottom: '1px solid #ccc',
+  background: 'transparent',
+  textDecoration: 'none',
+});
+
+const SpanItem = styled.span({
+  fontWeight: 'bold',
+  fontSize: '1.3em',
+});
+
+const Content = styled.div({
+  padding: '.3em',
+});
+
+const Span = styled.span({
+  paddingLeft: '0.5em',
+  fontWeight: 'bold',
+  fontSize: '1.1em',
+  cursor: 'pointer',
+});
 
 export default function RetrospectivesContainer() {
   // const retrospectives = useSelector(get('retrospectives'));
 
   return (
-    <li>
+    <List>
       {retrospectives.map((retrospective) => (
-        <ul key={retrospective.id}>
-          <div>
-            주제 :
-            {` ${retrospective.subject}`}
-          </div>
-          <div>
-            사실(fact) :
+        <Item key={retrospective.id}>
+          <Content>
+            <SpanItem>주제 :</SpanItem>
+            {` ${retrospective.subject} `}
+            {retrospective.filter.map((item) => (
+              // TODO : key should be added
+              <Span>
+                #
+                {item}
+              </Span>
+            ))}
+          </Content>
+          <Content>
+            <SpanItem>사실(Fact) :</SpanItem>
             {` ${retrospective.fact}`}
-          </div>
-          <div>
-            느낌(feeling) :
+          </Content>
+          <Content>
+            <SpanItem>느낌(Feeling) :</SpanItem>
             {` ${retrospective.feeling}`}
-          </div>
-          <div>
-            교훈(finding) :
+          </Content>
+          <Content>
+            <SpanItem>교훈(Finding) :</SpanItem>
             {` ${retrospective.finding}`}
-          </div>
-          <div>
-            향후 행동(future action) :
+          </Content>
+          <Content>
+            <SpanItem>향후 행동(Future action) :</SpanItem>
             {` ${retrospective.future_action}`}
-          </div>
-          <div>
-            피드백(feedback) :
+          </Content>
+          <Content>
+            <SpanItem>피드백(Feedback) :</SpanItem>
             {` ${retrospective.feedback}`}
-          </div>
-        </ul>
+          </Content>
+        </Item>
       ))}
-    </li>
+    </List>
   );
 }
